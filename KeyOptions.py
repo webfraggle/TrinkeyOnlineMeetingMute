70dc0c142771319f5113c9f1aed25df55e89a8f4
Christoph Ketzler <christoph@ketzler.de>
2021-09-26 17:27:00 +0200

    Added mac lockscreen, shortened descriptions

--- KeyOptions.py
+++ KeyOptions.py
@@ -2,23 +2,24 @@ from adafruit_hid.keycode import Keycode
 
 class KeyOptions():
     OPTIONS = (
-   {'keys': (Keycode.CONTROL, Keycode.SHIFT, Keycode.M), 'description': 'Windows Teams MuteUnmute Ctrl Shift M'},
-   {'keys': (Keycode.COMMAND, Keycode.SHIFT, Keycode.M), 'description': 'MAC Teams MuteUnmute Command Shift M'},
+   {'keys': (Keycode.CONTROL, Keycode.SHIFT, Keycode.M), 'description': 'Win Teams ToggleMute Ctrl Shift M'},
+   {'keys': (Keycode.COMMAND, Keycode.SHIFT, Keycode.M), 'description': 'Mac Teams ToggleMute Cmd Shift M'},
 
-   {'keys': (Keycode.ALT, Keycode.A), 'description': 'Windows Zoom MuteUnmute Alt A'},
-   {'keys': (Keycode.COMMAND, Keycode.SHIFT, Keycode.A), 'description': 'MAC Zoom MuteUnmute Command Shift A'},
+   {'keys': (Keycode.ALT, Keycode.A), 'description': 'Win Zoom ToggleMute Alt A'},
+   {'keys': (Keycode.COMMAND, Keycode.SHIFT, Keycode.A), 'description': 'Mac Zoom ToggleMute Cmd Shift A'},
 
-   {'keys': (Keycode.CONTROL, Keycode.M), 'description': 'Windows Skype Webex MuteUnmute Ctrl M'},
-   {'keys': (Keycode.COMMAND, Keycode.SHIFT, Keycode.M), 'description': 'MAC Skype Webex MuteUnmute Command Shift M'},
+   {'keys': (Keycode.CONTROL, Keycode.M), 'description': 'Win Skype Webex ToggleMute Ctrl M'},
+   {'keys': (Keycode.COMMAND, Keycode.SHIFT, Keycode.M), 'description': 'Mac Skype Webex ToggleMute Cmd Shift M'},
 
-   {'keys': (Keycode.CONTROL, Keycode.D), 'description': 'Windows GoogleMeet MuteUnmute Ctrl D'},
-   {'keys': (Keycode.COMMAND, Keycode.D), 'description': 'MAC GoogleMeet MuteUnmute Command D'},
+   {'keys': (Keycode.CONTROL, Keycode.D), 'description': 'Win GoogleMeet ToggleMute Ctrl D'},
+   {'keys': (Keycode.COMMAND, Keycode.D), 'description': 'Mac GoogleMeet ToggleMute Cmd D'},
 
-   {'keys': (Keycode.M), 'description': 'Windows Mac Jitsi MuteUnmute M'},
+   {'keys': (Keycode.M), 'description': 'Win Mac Jitsi ToggleMute M'},
 
-   {'keys': (Keycode.ALT, Keycode.SHIFT, Keycode.M), 'description': 'Windows BigBlueButton MuteUnmute Alt Shift M'},
-   {'keys': (Keycode.CONTROL, Keycode.OPTION, Keycode.M), 'description': 'MAC BigBlueButton MuteUnmute Control Option M'},
+   {'keys': (Keycode.ALT, Keycode.SHIFT, Keycode.M), 'description': 'Win BigBlueButton ToggleMute Alt Shift M'},
+   {'keys': (Keycode.CONTROL, Keycode.OPTION, Keycode.M), 'description': 'Mac BigBlueButton ToggleMute Ctrl Option M'},
 
 
-   {'keys': (Keycode.GUI, Keycode.L), 'description': 'Windows Lock Screen Win L'}
+   {'keys': (Keycode.GUI, Keycode.L), 'description': 'Win Lock Screen Win L'},
+   {'keys': (Keycode.CONTROL, Keycode.COMMAND, Keycode.Q), 'description': 'Mac Lock Screen Ctrl Cmd Q'}
 )
